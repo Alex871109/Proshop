528b2c675ba20a97bd91236695019927624059ae
Luis Alejandro <alex87gonz11@gmail.com>
2024-03-29 16:42:59 +0000

    feat: first commit de la tarde

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,29 @@
-import express from 'express';
-import products from './data/products.js';
-import dotenv from 'dotenv';
+import express from "express";
+import products from "./data/products.js";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 dotenv.config();
 const port = process.env.PORT || 5000;
 
+
+connectDB();
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hello from simple server :)');
+app.get("/", (req, res) => {
+  res.send("hello from simple server :)");
 });
 
-app.get('/api/products', (req, res) => {
+app.get("/api/products", (req, res) => {
   res.json(products);
 });
 
-app.get('/api/products/:id', (req, res) => {
+app.get("/api/products/:id", (req, res) => {
   const id = req.params.id;
   const product = products.find((p) => p._id === id);
   res.json(product);
 });
 
 app.listen(port, () =>
-  console.log('> Server is up and running on port : ' + port)
+  console.log("> Server is up and running on port : " + port)
 );
